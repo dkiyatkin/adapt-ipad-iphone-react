c30378a70ac93fe3f5d1714d7887eab372ae037f
Dmitriy Kiyatkin <dkiyatkin@gmail.com>
2020-07-31 01:45:06 +0400

    clock

---
+++ src/components/Workspace/StatusBar/Clock.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import cn from 'classnames'
+
+import styles from './Clock.module.scss'
+
+export default class Clock extends React.Component {
+  static propTypes = {
+    className: PropTypes.string,
+  }
+
+  constructor (props) {
+    super(props)
+    this.runner = null
+    this.state = {
+      time: this.getCurrentTime()
+    }
+  }
+
+  getCurrentTime () {
+    return new Date().toLocaleTimeString('ru-RU')
+  }
+
+  componentDidMount () {
+    this.runner = setInterval(() => {
+      this.setState({ time: this.getCurrentTime() })
+    }, 1000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.runner)
+  }
+
+  render () {
+    return (
+      <div className={cn(styles.clock, this.props.className)}>
+        {this.state.time}
+      </div>
+    )
+  }
+}
--- src/components/Workspace/StatusBar/index.js
+++ src/components/Workspace/StatusBar/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import cn from 'classnames'
+import Clock from './Clock'
 
 import styles from './index.module.scss'
 
 export default function StatusBar (props) {
   return (
     <div className={cn(styles.statusBar, props.className)}>
-      time
+      <Clock />
     </div>
   )
 }
